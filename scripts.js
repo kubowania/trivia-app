f70f06373878e24fa09afcc8184b043ebf0b9a6a
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2021-12-22 16:19:00 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,117 @@
+const game = document.getElementById('game')
+const scoreDisplay = document.getElementById('score')
+let score = 0
+
+const genres = [
+    {
+        name: 'Books',
+        id: 10
+    },
+    {
+        name: 'Film',
+        id: 11
+    },
+    {
+        name: 'Music',
+        id: 12
+    },
+    {
+        name: 'Video Games',
+        id: 15
+    }
+]
+
+const levels = ['easy', 'medium', 'hard']
+
+function addGenre(genre) {
+    const column = document.createElement('div')
+    column.classList.add('genre-column')
+    column.innerHTML = genre.name
+    game.append(column)
+
+    levels.forEach(level => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        column.append(card)
+
+        if (level === 'easy') {
+            card.innerHTML = 100
+        }
+        if (level === 'medium') {
+            card.innerHTML = 200
+        }
+        if (level === 'hard') {
+            card.innerHTML = 300
+        }
+
+        fetch(`https://opentdb.com/api.php?amount=1&category=${genre.id}&difficulty=${level}&type=boolean`)
+            .then(response => response.json())
+            .then(data => {
+                // Not enough data to get 10 items per difficulty level and get a random
+                // question using Math.random() and passing it through instead of 0
+                // eg: if amount=10 above, you could do:
+                // cont randomNumber = Math.floor(Math.random() * 10)
+                // and pass through randomNumber, so:
+                // data.results[randomNumber].question
+                console.log(data)
+                card.setAttribute('data-question', data.results[0].question)
+                card.setAttribute('data-answer', data.results[0].correct_answer)
+                card.setAttribute('data-value', card.getInnerHTML())
+            })
+            .then(done => card.addEventListener('click', flipCard))
+
+    })
+}
+genres.forEach(genre => addGenre(genre))
+
+
+function flipCard() {
+    this.innerHTML = ''
+    this.style.fontSize = '15px'
+    const textDisplay = document.createElement('div')
+    const trueButton = document.createElement('button')
+    const falseButton = document.createElement('button')
+    trueButton.innerHTML = 'True'
+    falseButton.innerHTML = 'False'
+    trueButton.classList.add('true-button')
+    falseButton.classList.add('false-button')
+    trueButton.addEventListener('click', getResult)
+    falseButton.addEventListener('click', getResult)
+    textDisplay.innerHTML = this.getAttribute('data-question')
+    this.append(textDisplay, trueButton, falseButton)
+
+    const allCards = Array.from(document.querySelectorAll('.card'))
+    allCards.forEach(card => card.removeEventListener('click', flipCard))
+}
+
+function getResult() {
+    const allCards = Array.from(document.querySelectorAll('.card'))
+    allCards.forEach(card => card.addEventListener('click', flipCard))
+
+    const cardOfButton = this.parentElement
+    if (cardOfButton.getAttribute('data-answer') === this.innerHTML) {
+        score = score + parseInt(cardOfButton.getAttribute('data-value'))
+        scoreDisplay.innerHTML = score
+        cardOfButton.classList.add('correct-answer')
+        setTimeout(() => {
+            while (cardOfButton.firstChild) {
+                cardOfButton.removeChild(cardOfButton.lastChild)
+            }
+            cardOfButton.innerHTML = cardOfButton.getAttribute('data-value')
+        }, 100)
+    } else {
+        cardOfButton.classList.add('wrong-answer')
+        setTimeout(() => {
+            while (cardOfButton.firstChild) {
+                cardOfButton.removeChild(cardOfButton.lastChild)
+            }
+            cardOfButton.innerHTML = 0
+        }, 100)
+    }
+    cardOfButton.removeEventListener('click',flipCard)
+}
+
+
+
+
+
